c510366051f8809b9aa8593d5b4db4eb6408cff1
Marylise Larouche <MALAR563@ulaval.ca>
2025-01-29 12:44:35 -0500

    diffusion fonctionnelle

--- temperature_plaque.py
+++ temperature_plaque.py
@@ -4,12 +4,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 import copy
+from tqdm import tqdm
 #changement
 
 #double changement
 
 class Plaque:
-    def __init__(self, dimensions=(0.117, 0.061), epaisseur=0.001, resolution_x=0.0001, resolution_t=1, T_ambiante=25, densite=2699, cap_calorifique=900, conduc_thermique=237, coef_convection=0):
+    def __init__(self, dimensions=(0.117, 0.061), epaisseur=0.001, resolution_x=0.0001, resolution_t=1, T_ambiante=25, densite=2699, cap_calorifique=900, conduc_thermique=237, coef_convection=20):
         self.dim = dimensions
         self.e = epaisseur
         self.dx = resolution_x
@@ -22,7 +23,7 @@ class Plaque:
         self.grille = self.T_amb*np.ones((int(self.dim[0]/self.dx), int(self.dim[1]/self.dx)))
         self.points_chauffants = []
         self.alpha = self.k/(self.rho*self.cp)
-        self.dt = (0.5*self.dx**2)/self.alpha
+        self.dt = (self.dx**2)/(self.alpha*8)
 
 
     def show(self):
@@ -48,47 +49,36 @@ class Plaque:
         Returns:
         chambre_nouvelle_petite (numpy.ndarray) : Chambre contenant le potentiel de l'itération suivante.
         """
-        # # Matrice non décalée
-        # T_centre = np.zeros((self.grille.shape[0]+2, self.grille.shape[1]+2))
-        # T_centre[1:-1, 1:-1] = self.grille
 
-        # # Matrice décalée vers le haut
-        # T_haut = np.zeros((self.grille.shape[0]+2, self.grille.shape[1]+2))
-        # T_haut[0:-2, 1:-1] = self.grille
-
-        # # Matrice décalée vers le bas
-        # T_bas = np.zeros((self.grille.shape[0]+2, self.grille.shape[1]+2))
-        # T_bas[2:, 1:-1] = self.grille
-
-        # # Matrice décalée vers la gauche
-        # T_gauche = np.zeros((self.grille.shape[0]+2, self.grille.shape[1]+2))
-        # T_gauche[1:-1, 0:-2] = self.grille
-
-        # # Matrice décalée vers la gauche
-        # T_droite = np.zeros((self.grille.shape[0]+2, self.grille.shape[1]+2))
-        # T_droite[1:-1, 2:] = self.grille
- 
-        # # Le potentiel à l'itération suivante est calculé selon l'expression trouvée à la question 1a)
-        # nouvelle_grille = (self.alpha*self.dt*(T_droite + T_gauche + T_bas + T_haut - 4*T_centre)/(self.dx**2)) + T_centre
-
-        # # Restreindre la chambre à sa grandeur d'origine
-        # self.grille = nouvelle_grille[1:-1, 1:-1]
-
-        "# CODE OPTIMISÉ"
         big_grille = np.zeros((self.grille.shape[0]+2, self.grille.shape[1]+2))
         big_grille[1:-1, 1:-1] = copy.copy(self.grille)
-        new_grille = (self.alpha*self.dt*(big_grille[2:, 1:-1] + big_grille[:-2, 1:-1] + big_grille[1:-1, 2:] +  big_grille[1:-1, :-2] - 4*big_grille[1:-1, 1:-1])/(self.dx**2)) + big_grille[1:-1, 1:-1]
+        diffusion = (self.alpha*self.dt*(big_grille[2:, 1:-1] + big_grille[:-2, 1:-1] + big_grille[1:-1, 2:] +  big_grille[1:-1, :-2] - 4*big_grille[1:-1, 1:-1])/(self.dx**2)) 
+        
+        # CF
+        self.grille[0, :] = 50    # Bord haut
+        self.grille[-1, :] = 50   # Bord bas
+        self.grille[:, 0] = 50    # Bord gauche
+        self.grille[:, -1] = 50
+
+        # # Convection (mettre *2 car rentre de 2 côtés?)
+        # convection = self.h * self.dx**2 * (self.T_amb - self.grille) / (self.rho * self.cp * self.dx**2 * self.e)
+
+        new_grille = self.grille + diffusion #+ convection
         self.grille = new_grille
         return self.grille
+    
 
 
 
-Ma_plaque = Plaque(resolution_t=0.01,T_ambiante=297)
 
-Ma_plaque.deposer_T(307, (0.10, 0.04))
-Ma_plaque.deposer_T(293, (0.02, 0.02))
+Ma_plaque = Plaque(T_ambiante=26)
+
+Ma_plaque.deposer_T(40, (0.10, 0.04))
+Ma_plaque.deposer_T(12, (0.02, 0.02))
 Ma_plaque.iteration()
 Ma_plaque.show()
 Ma_plaque.iteration()
 Ma_plaque.show()
-
+for n in tqdm(range(2500)):
+    Ma_plaque.iteration()
+Ma_plaque.show()
